136a6f939a5d95c523b65ca3b71902b1a170deb2
taras <nomail@nomail>
2024-08-12 21:48:02 +0200

    Add windows

--- picows/picows.pyx
+++ picows/picows.pyx
@@ -32,12 +32,31 @@ cdef extern from "arpa/inet.h" nogil:
 
 cdef extern from * nogil:
     """
+    #if (defined(_WIN16) || defined(_WIN32) || defined(_WIN64)) && !defined(__WINDOWS__)
+    #	define __WINDOWS__
+    #endif
+
     #if defined(__linux__)
       #include <endian.h>
     #elif defined(__APPLE__)
       #include <libkern/OSByteOrder.h>
       #define be64toh(x) OSSwapBigToHostInt64(x)
       #define htobe64(x) OSSwapHostToBigInt64(x)
+    #elif defined(__OpenBSD__)
+      #include <sys/endian.h>
+    #elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
+      #include <sys/endian.h>
+      #define be64toh(x) betoh64(x)
+    #elif defined(__WINDOWS__)
+      #if BYTE_ORDER == LITTLE_ENDIAN
+        #define be64toh(x) ntohll(x)
+        #define htobe64(x) htonll(x)
+      #elif BYTE_ORDER == BIG_ENDIAN
+        #define be64toh(x) (x)
+        #define htobe64(x) (x)
+      #endif
+    #else
+      error byte order not supported
     #endif
     """
     # Network order is big-endian
