5bfb45040f34168060f4e490116c63a5a71f7ffa
taras <nomail@nomail>
2024-08-15 14:22:38 +0200

    Attempt to fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../picows/'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 # Configuration file for the Sphinx documentation builder.
 #
