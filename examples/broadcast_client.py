442121f0c2cee0e7392606e337c05d219d601cbe
taras <nomail@nomail>
2025-02-09 16:04:48 +0100

    Add broadcast examples

---
+++ examples/broadcast_client.py
@@ -0,0 +1,19 @@
+import asyncio
+from picows import ws_connect, WSFrame, WSTransport, WSListener, WSMsgType, WSCloseCode
+
+
+class ClientListener(WSListener):
+    def on_ws_connected(self, transport: WSTransport):
+        print("Client connected")
+
+    def on_ws_frame(self, transport: WSTransport, frame: WSFrame):
+        print(f"Received: {frame.get_payload_as_ascii_text()}")
+
+
+async def main(url):
+    transport, client = await ws_connect(ClientListener, url)
+    await transport.wait_disconnected()
+
+
+if __name__ == '__main__':
+    asyncio.run(main("ws://127.0.0.1:9001"))
---
+++ examples/broadcast_server.py
@@ -0,0 +1,72 @@
+# This example shows how to maintain a set of active clients and broadcast messages to everybody.
+# The set contains weak references to clients, it is done in order to prevent client from holding references
+# to other clients when server is dead.
+
+
+import asyncio
+import itertools
+from weakref import ref, ReferenceType
+
+from picows import ws_create_server, WSFrame, WSTransport, WSListener, \
+    WSMsgType, WSUpgradeRequest
+
+
+class ServerClientListener(WSListener):
+    def __init__(self, all_clients):
+        self.transport = None
+        self._all_clients = all_clients
+
+    def on_ws_connected(self, transport: WSTransport):
+        self.transport = transport
+        self._all_clients.add(ref(self))
+
+    def on_ws_disconnected(self, transport: WSTransport):
+        self._all_clients.remove(ref(self))
+
+    def on_ws_frame(self, transport: WSTransport, frame: WSFrame):
+        if frame.msg_type == WSMsgType.CLOSE:
+            transport.send_close(frame.get_close_code(), frame.get_close_message())
+            transport.disconnect()
+
+        # ...
+        # Custom protocol logic
+
+
+class Server:
+    _all_clients: set[ReferenceType[ServerClientListener]]
+
+    def __init__(self):
+        self._all_clients = set()
+
+    def broadcast(self, message):
+        for client_ref in self._all_clients:
+            client = client_ref()
+            if client is not None:
+                client.transport.send(WSMsgType.TEXT, message)
+
+    async def run(self):
+        def listener_factory(r: WSUpgradeRequest):
+            return ServerClientListener(self._all_clients)
+
+        server: asyncio.Server = await ws_create_server(listener_factory,
+                                                        "127.0.0.1", 9001)
+        for s in server.sockets:
+            print(f"Server started on {s.getsockname()}")
+
+        await server.serve_forever()
+
+
+async def main():
+    server = Server()
+
+    async def broadcast_something_every_second():
+        for i in itertools.count():
+            await asyncio.sleep(1)
+            server.broadcast(b"Hello, this is broadcast number #%d" % (i, ))
+            print(f"Broadcasted message #{i}")
+
+    await asyncio.gather(server.run(), broadcast_something_every_second())
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
