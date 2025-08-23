fb030cc2bdde13eaa4dff73bd66c5b06c880394e
taras <nomail@nomail>
2025-08-23 16:12:53 +0200

    Add -g0 to cibuildwheel

--- setup.py
+++ setup.py
@@ -2,40 +2,15 @@ import os
 import sys
 from Cython.Build import cythonize
 from setuptools import Extension, setup
-from setuptools.command.build_ext import build_ext
 
 vi = sys.version_info
 if vi < (3, 9):
     raise RuntimeError('picows requires Python 3.9 or greater')
 
 
-# class CustomBuildExt(build_ext):
-#     def build_extensions(self):
-#         for ext in self.extensions:
-#             if ext.extra_compile_args:
-#                 ext.extra_compile_args = [
-#                     flag for flag in ext.extra_compile_args if flag != '-g'
-#                 ]
-#             if ext.extra_link_args:
-#                 ext.extra_link_args = [
-#                     flag for flag in ext.extra_link_args if flag != '-g'
-#                 ]
-#
-#             if os.name != 'nt':
-#                 self.compiler.compiler_so = [
-#                     flag for flag in self.compiler.compiler_so if flag != '-g'
-#                 ]
-#                 self.compiler.linker_so = [
-#                     flag for flag in self.compiler.linker_so if flag != '-g'
-#                 ]
-#         super().build_extensions()
-#
-
 if os.name == 'nt':
-    link_args = []
     libraries = ["Ws2_32"]
 else:
-    link_args = ["-Wl,-s"]
     libraries = None
 
 cython_modules = [
@@ -51,12 +26,10 @@ if os.getenv("PICOWS_BUILD_EXAMPLES") is not None:
         Extension(
             "examples.echo_client_cython",
             ["examples/echo_client_cython.pyx"],
-            extra_link_args=link_args,
         )
     )
 
 setup(
-    # cmdclass={'build_ext': CustomBuildExt},
     ext_modules=cythonize(
         cython_modules,
         compiler_directives={
