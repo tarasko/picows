3433e9741c1414bb6e7d8b4aace4d90a005f6c84
taras <nomail@nomail>
2025-08-27 11:48:36 +0200

    Fix windows build

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ example_extensions = [
     Extension("examples.echo_client_cython", ["examples/echo_client_cython.pyx"], libraries=libraries),
 ]
 
-is_wheel = any(cmd in sys.argv for cmd in ("bdist_wheel",))
-extensions = pkg_extensions if is_wheel else (pkg_extensions + example_extensions)
+build_wheel = any(cmd in sys.argv for cmd in ("bdist_wheel",))
+extensions = (pkg_extensions + example_extensions) if not build_wheel and os.name != 'nt' else pkg_extensions
 
 setup(
     ext_modules=cythonize(
