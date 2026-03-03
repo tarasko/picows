5c38b22c6e96a603388f0b68b7aa24f93addc8a5
taras <nomail@nomail>
2026-03-03 12:00:24 +0100

    Try building everywhere

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import os
+import shlex
 import sys
+import sysconfig
+from pathlib import Path
 from Cython.Build import cythonize
 from setuptools import Extension, setup
 
@@ -8,28 +11,105 @@ if vi < (3, 9):
     raise RuntimeError('picows requires Python 3.9 or greater')
 
 
-if os.name == 'nt':
-    libraries = ["Ws2_32"]
+is_windows = os.name == 'nt'
+
+if is_windows:
+    base_libraries = ["Ws2_32"]
 else:
-    libraries = ["ssl", "crypto"]
+    base_libraries = []
 
 extra_compile_args = []
 
+
+def _parse_ssl_flags() -> tuple[list[str], list[str], list[str], list[str], list[str]]:
+    include_dirs: list[str] = []
+    compile_args: list[str] = []
+    library_dirs: list[str] = []
+    libraries: list[str] = []
+    link_args: list[str] = []
+
+    cflags = sysconfig.get_config_var("MODULE__SSL_CFLAGS") or ""
+    ldflags = sysconfig.get_config_var("MODULE__SSL_LDFLAGS") or ""
+
+    for token in shlex.split(cflags):
+        if token.startswith("-I"):
+            include_dirs.append(token[2:])
+        elif token.startswith("/I"):
+            include_dirs.append(token[2:])
+        else:
+            compile_args.append(token)
+
+    for token in shlex.split(ldflags):
+        if token.startswith("-L"):
+            library_dirs.append(token[2:])
+        elif token.startswith("/LIBPATH:"):
+            library_dirs.append(token[len("/LIBPATH:"):])
+        elif token.startswith("-l"):
+            libraries.append(token[2:])
+        elif token.lower().endswith((".lib", ".a", ".so", ".dylib", ".dll")):
+            link_args.append(token)
+        else:
+            link_args.append(token)
+
+    include_dirs = [path for path in include_dirs if Path(path).exists()]
+    library_dirs = [path for path in library_dirs if Path(path).exists()]
+
+    if not include_dirs:
+        include_candidates = [
+            Path(sys.base_prefix) / "include",
+            Path(sys.prefix) / "include",
+            Path(sys.exec_prefix) / "include",
+            Path(sys.base_prefix) / "Library" / "include",
+            Path(sys.prefix) / "Library" / "include",
+        ]
+        for candidate in include_candidates:
+            if (candidate / "openssl" / "ssl.h").exists():
+                include_dirs.append(str(candidate))
+                break
+
+    if not library_dirs:
+        lib_candidates = [
+            Path(sys.base_prefix) / "lib",
+            Path(sys.prefix) / "lib",
+            Path(sys.exec_prefix) / "lib",
+            Path(sys.base_prefix) / "libs",
+            Path(sys.prefix) / "libs",
+            Path(sys.exec_prefix) / "libs",
+            Path(sys.base_prefix) / "DLLs",
+            Path(sys.prefix) / "DLLs",
+        ]
+        existing = [str(path) for path in lib_candidates if path.exists()]
+        library_dirs.extend(existing)
+
+    if not libraries:
+        libraries.extend(["ssl", "crypto"] if not is_windows else ["libssl", "libcrypto"])
+
+    return include_dirs, compile_args, library_dirs, libraries, link_args
+
+
+ssl_include_dirs, ssl_compile_args, ssl_library_dirs, ssl_libraries, ssl_link_args = _parse_ssl_flags()
+sslproto_sources = ["aiofastnet/sslproto.pyx", "aiofastnet/static_mem_bio.c", "aiofastnet/certdecode.c"]
+sslproto_libraries = base_libraries + ssl_libraries
+
 pkg_extensions = [
     Extension("picows.picows", ["picows/picows.pyx"],
-              libraries=libraries, extra_compile_args=extra_compile_args),
+              libraries=base_libraries, extra_compile_args=extra_compile_args),
     Extension("aiofastnet.utils", ["aiofastnet/utils.pyx"],
-              libraries=libraries, extra_compile_args=extra_compile_args),
+              libraries=base_libraries, extra_compile_args=extra_compile_args),
     Extension("aiofastnet.transport", ["aiofastnet/transport.pyx"],
-              libraries=libraries, extra_compile_args=extra_compile_args),
-    Extension("aiofastnet.sslproto", ["aiofastnet/sslproto.pyx", "aiofastnet/static_mem_bio.c", "aiofastnet/certdecode.c"],
-              libraries=libraries, extra_compile_args=extra_compile_args),
+              libraries=base_libraries, extra_compile_args=extra_compile_args),
+    Extension("aiofastnet.sslproto", sslproto_sources,
+              include_dirs=ssl_include_dirs,
+              library_dirs=ssl_library_dirs,
+              libraries=sslproto_libraries,
+              extra_compile_args=extra_compile_args + ssl_compile_args,
+              extra_link_args=ssl_link_args),
     Extension("aiofastnet.sslproto_stdlib", ["aiofastnet/sslproto_stdlib.pyx"],
-              libraries=libraries, extra_compile_args=extra_compile_args),
+              libraries=base_libraries, extra_compile_args=extra_compile_args),
 ]
 
 example_extensions = [
-    Extension("examples.echo_client_cython", ["examples/echo_client_cython.pyx"], libraries=libraries),
+    Extension("examples.echo_client_cython", ["examples/echo_client_cython.pyx"], libraries=base_libraries),
 ]
 
 build_wheel = any(cmd in sys.argv for cmd in ("bdist_wheel",))
