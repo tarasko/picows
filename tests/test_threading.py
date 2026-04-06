2aea0220118c1bfdcc6f9997e7205bd1de9bda8e
taras <nomail@nomail>
2026-04-07 00:23:34 +0200

    Update tests

--- tests/test_threading.py
+++ tests/test_threading.py
@@ -1,3 +1,4 @@
+import asyncio
 from concurrent.futures import ThreadPoolExecutor
 
 import pytest
@@ -7,6 +8,7 @@ from tests.utils import WSServer, WSClient
 
 
 async def test_wrong_thread_assert():
+    loop = asyncio.get_running_loop()
     with ThreadPoolExecutor(max_workers=1) as executor:
         async with WSServer() as server:
             async with WSClient(server) as client:
@@ -14,19 +16,19 @@ async def test_wrong_thread_assert():
                 msg_ba = bytearray(b"asasdfbasdfbaskjdfasd")
 
                 with pytest.raises(RuntimeError, match="WSTransport.send called from a wrong thread"):
-                    executor.submit(client.transport.send, WSMsgType.BINARY, msg).result()
+                    await loop.run_in_executor(executor, client.transport.send, WSMsgType.BINARY, msg)
 
                 with pytest.raises(RuntimeError, match="WSTransport.send_ping called from a wrong thread"):
-                    executor.submit(client.transport.send_ping).result()
+                    await loop.run_in_executor(executor, client.transport.send_ping)
 
                 with pytest.raises(RuntimeError, match="WSTransport.send_pong called from a wrong thread"):
-                    executor.submit(client.transport.send_pong).result()
+                    await loop.run_in_executor(executor, client.transport.send_pong)
 
                 with pytest.raises(RuntimeError, match="WSTransport.send_close called from a wrong thread"):
-                    executor.submit(client.transport.send_close).result()
+                    await loop.run_in_executor(executor, client.transport.send_close)
 
                 with pytest.raises(RuntimeError, match="WSTransport.send_reuse_external_bytearray called from a wrong thread"):
-                    executor.submit(client.transport.send_reuse_external_bytearray, WSMsgType.BINARY, msg_ba, 14).result()
+                    await loop.run_in_executor(executor, client.transport.send_reuse_external_bytearray, WSMsgType.BINARY, msg_ba, 14)
 
                 with pytest.raises(RuntimeError, match="WSTransport.disconnect called from a wrong thread"):
-                    executor.submit(client.transport.disconnect).result()
+                    await loop.run_in_executor(executor, client.transport.disconnect)
